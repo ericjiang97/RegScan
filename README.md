5af92a42fa3cfe729ba8e97955d39d9bd2279bfb
JIANG, Eric <jia0020@camhigh.vic.edu.au>
2014-08-14 09:59:01 +1000

    transfer from PhantomLabs\RegScan to JIA0020\RegScan
    - transfer to private repo

--- README.md
+++ README.md
@@ -1,6 +1,42 @@
-RegScan
-=======
+RegScan (v1.1a) Black Titanium
+======= 
+A Registry Scanner
 
-A Registry Scanner, Private REPO
+Current Update Notes
+-------
+- RegScan scans in WindowsStore
+- Runs with Administrative Privallages (HighestPossible)
 
-Transfering Files Over Soon.
+General Usage Notes
+-------
+- RegScan is to be used as a background program. Scanning for changes in the Registry (regedit.exe) on targeted registry keys. It scans the following keys:
+    - HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\WindowsStore\\WindowsStore (INT)
+    - HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System\\DisableChangePassword (DWORD)
+- RegScan is also used to block changing of the Local Administrator (.\administrator) Password
+
+
+
+Documentation:
+-------
+http://phantomlabs.github.io/RegScan
+http://github.com/PhantomLabs/RegScan/Wiki
+
+
+Installation
+-------
+- install with Source
+- Install with commerical *.exe (downloadable within next update)
+
+License
+-------
+This Program is licensed under the Apache Version 2.0 License. See LICENSE for more details.
+Also Protected with EULA license.
+
+Planned Features/Debugging (Notes for Developers)
+- latest version is unstable (redeveloping core)
+- need to find a way to block Administrator Password, registry?
+- license Registry Status
+
+
+
+
---
+++ RScan/Program.cs
@@ -0,0 +1,112 @@
+﻿#region RScan Details
+/*
+ * RScan By PhantomLabs
+ * Source Code: Version 1.0a (Alpha 1) - Black Titanium
+ * License-Type: Open-Source (CODE ONLY) | Commercial License (PROGRAM)
+ * Customisable - Request Features via Email.
+ * 
+ * By Eric Jiang
+ * Editors: Evan Petousis, Eric Jiang
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace RScan
+{
+    internal static class Program
+    {
+        public static string pname = "RegScan";
+
+        private static void Main(string[] args)
+        {
+            #region RScan_licensestatus
+            int licensestatus = 0; //0 - DefaultValue (Unactivated)
+            
+            /*
+             * If RegistryKey = 1, then is activated.
+             * Planned Registry Path: Software\\PhantomLabs\\RegScan
+             * int/obj "License" = 1
+             */
+
+            #endregion
+
+            #region Reg: WindowsStoreScanner
+            /*
+             * WindowsStoreScanner Blocker Version 1.0.1b
+             * Original Program by Eric Jiang
+             * Version 1.0.1a edited by Evan Petousis
+             */
+
+            try
+            {
+                //HKEY_CURRENT_USER\Software\Policies\Microsoft\WindowsStore
+                RegistryKey winstorekey = Registry.LocalMachine.OpenSubKey("Software\\Policies\\Microsoft\\WindowsStore");
+                const string winstorekeyname = "HKEY_CURRENT_USER\\Software\\Policies\\Microsoft\\WindowsStore";
+                object keyvalue = winstorekey.GetValue("WindowsStore");
+                int keyvalueint = Convert.ToInt32(keyvalue);
+                if (keyvalueint == 0) 
+                {
+                    Object o = winstorekey.GetValue("RemoveWindowsStore");
+                    //WindowsStore Unblocked
+                    try
+                    {
+                        //RemoveWindowsStore is '0'. Reset value to 1
+                        Registry.SetValue(winstorekeyname, "RemoveWindowsStore", "1", RegistryValueKind.DWord);
+                        winstorekey.SetValue("RemoveWindowsStore","1");
+                        //Remove(?): key.SetValue("RemoveWindowsStore", "1", RegistryValueKind.String);
+                        MessageBox.Show("WindowsStore from 0 to 1 Successful", pname, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Object reg = winstorekey.GetValue("RemoveWindowsStore");
+                        MessageBox.Show(winstorekeyname + " Value = " + keyvalueint, pname, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }//try_if
+
+                    catch
+                    {
+                            MessageBox.Show("WindowsStore Value: " + keyvalueint + ". An Error Has Occured - Failed to write Registry Entry", "RSCan");
+                    }//catch_if
+                }
+
+            }
+            catch
+            {
+                //ignore
+            }
+
+            #endregion
+
+            #region Reg: DisableChangePassword
+            /*
+             * DisableChangePassword
+             * Group Block Version: 0.1a
+             */
+            try
+            {
+                RegistryKey dcpKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System");
+                const string dcpkeyname = "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\System";
+                object keyvalue = dcpKey.GetValue("DisableChangePassword");
+                int keyvalueint = Convert.ToInt32(keyvalue);
+                if (keyvalueint == 0)
+                {
+                    
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            #endregion
+
+            #region adminblock
+
+            #endregion
+        }
+    }
+}
